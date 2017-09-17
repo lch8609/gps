0fae6e50e3d8c9af9837c3b8d70bde5d92e52fc4
lch8609 <com_engineer@naver.com>
2017-09-17 22:22:57 +0900

    gps 권한설정 잘되게 수정완료

--- app/src/main/java/com/thatzit/leechanghyeon/gpsexample/MainActivity.java
+++ app/src/main/java/com/thatzit/leechanghyeon/gpsexample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.thatzit.leechanghyeon.gpsexample;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
@@ -7,10 +9,12 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,15 +24,41 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private LocationManager locationManager;
     private String provider;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if ( Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    0 );
+//    static final int LOCATION_REQUEST_CODE = 101;
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode) {
+//            case LOCATION_REQUEST_CODE: {
+//                if (grantResults.length == 0 ||
+//                        grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+//                    Toast.makeText(this, "Unable to show location - permission requerd", Toast.LENGTH_LONG).show();
+//                }
+//                return;
+//            }
+//        }
+//        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//    }
+//
+//    protected void requestPermission(String permissionType, int requestCode) {
+//        int permission = ContextCompat.checkSelfPermission(this, permissionType);
+//
+//        if (permission != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{permissionType}, requestCode);
+//        }
+//    }
+
+    void GPSinit(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
-        setContentView(R.layout.activity_main);
         latituteField = (TextView) findViewById(R.id.TextView02);
         longitudeField = (TextView) findViewById(R.id.TextView04);
         // Get the location manager
@@ -37,6 +67,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         // default
         Criteria criteria = new Criteria();
         provider = locationManager.getBestProvider(criteria, false);
+
+
         Location location = locationManager.getLastKnownLocation(provider);
 
         // Initialize the location fields
@@ -47,10 +79,43 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             latituteField.setText("Location not available");
             longitudeField.setText("Location not available");
         }
+
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("test", "onCreate");
+        super.onCreate(savedInstanceState);
+
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION},1000);
+                }
+            //requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_REQUEST_CODE);
+            }
+
+
+
+
+//        if ( Build.VERSION.SDK_INT >= 23 &&
+//                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+//            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
+//                    0 );
+//        }
+
+        GPSinit();
+        setContentView(R.layout.activity_main);
+
+
+
     }
 
     @Override
     protected void onResume() {
+        Log.d("test", "onResume");
         super.onResume();
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -62,17 +127,67 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
+        try {
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }catch (NullPointerException ex){
+            Log.d("test", "nullpointexception : "+ex.toString());
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            latituteField = (TextView) findViewById(R.id.TextView02);
+            longitudeField = (TextView) findViewById(R.id.TextView04);
+            // Get the location manager
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            // Define the criteria how to select the locatioin provider -> use
+            // default
+            Criteria criteria = new Criteria();
+            provider = locationManager.getBestProvider(criteria, false);
+
+
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            // Initialize the location fields
+            if (location != null) {
+                System.out.println("Provider " + provider + " has been selected.");
+                onLocationChanged(location);
+            } else {
+                latituteField.setText("Location not available");
+                longitudeField.setText("Location not available");
+            }
+
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
+
     }
 
     @Override
     protected void onPause() {
+        Log.d("test", "onPause");
         super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locationManager.removeUpdates(this);
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        Log.d("test", "onLocationChanged");
         double lat = (location.getLatitude());
         double lng = (location.getLongitude());
         latituteField.setText(String.valueOf(lat));
@@ -81,17 +196,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
-
+        Log.d("test", "onStatusChanged");
     }
 
     @Override
     public void onProviderEnabled(String s) {
+        Log.d("test", "onProviderEnabled");
         Toast.makeText(this, "Enabled new provider " + provider,
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onProviderDisabled(String s) {
+        Log.d("test", "onProviderDisabled");
         Toast.makeText(this, "Disabled provider " + provider,
                 Toast.LENGTH_SHORT).show();
     }
